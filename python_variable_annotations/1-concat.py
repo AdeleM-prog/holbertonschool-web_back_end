121742e3c36da17e8601c0dd3ceb1ee77e2be8a1
AdeleM-prog <12043@holbertonstudents.com>
2026-04-07 10:09:48 +0200

    crea 1-concat.py + 1-main.py + implemented function to concat str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" Module that defines a function to concatenate two strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result.
+
+    Args:
+        str1: The first string to concatenate.
+        str2: The second string to concatenate.
+
+    Returns:
+        The concatenated string of str1 and str2.
+    """
+    return str1 + str2
