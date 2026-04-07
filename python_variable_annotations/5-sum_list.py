b49d11b339159afeeaac7a1de6402383182cf1f6
AdeleM-prog <12043@holbertonstudents.com>
2026-04-07 10:46:31 +0200

    crea 5-sum_list + 5-main + pycodestyle

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from typing import List
+"""Module that defines a function to compute the sum of a list of floats."""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of a list of floats.
+    Args:
+        input_list: A list of float numbers to be summed.
+
+    Returns:
+        The sum of all the floats in the list.
+    """
+    return sum(input_list)
