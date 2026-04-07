b3a8bbeaa32678d7e5309c0f843dada1798b9fa3
AdeleM-prog <12043@holbertonstudents.com>
2026-04-07 14:36:14 +0200

    crea 0-main + 0-basic_async_syntax + pycodestyle

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import random
+import asyncio
+"""Module for asynchronous tasks with random delay."""
+
+
+async def wait_random(max_delay=10):
+    """
+    Waits for a random delay between 0 and max_delay seconds.
+    Args:
+        max_delay (int): The maximum delay in seconds (default is 10).
+    Returns:
+        float: The random delay that was waited.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
