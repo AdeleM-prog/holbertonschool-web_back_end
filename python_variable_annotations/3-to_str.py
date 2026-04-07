0389f406123fc50f9fd68d4e2fab6cea6a5c4dbf
AdeleM-prog <12043@holbertonstudents.com>
2026-04-07 10:26:12 +0200

    crea 3-main + 3-to_str + pycodestyle

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Module that defines a function to convert a float to
+its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation.
+    Args:
+        n: The float number to convert.
+
+    Returns:
+        The string representation of the float.
+    """
+    return str(n)
