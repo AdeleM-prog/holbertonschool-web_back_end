d6c2fef6d896a9b24012cd43adc3ec8dad5450b9
AdeleM-prog <12043@holbertonstudents.com>
2026-04-07 11:14:35 +0200

    crea 7-main + 7-to_kv + pycodestyle

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from typing import Union, Tuple
+"""Module that defines a function to map a string to a squared value."""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with a string and the square of a number as float.
+    Args:
+        k: The string key.
+        v: An int or float value.
+
+    Returns:
+        A tuple with the string and the squared value as float.
+    """
+    v = float(v * v)
+    return (k, v)
