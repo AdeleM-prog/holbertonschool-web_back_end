14fd620cac1a26676b02d1dfd33df11825eda6f5
AdeleM-prog <12043@holbertonstudents.com>
2026-04-09 17:57:23 +0200

    correction pycodeystyle

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""module that provides a function to measure the execution time of a process"""
+"""module that provides a function to measure the execution time of
+a process"""
 
-wait_n =__import__('1-concurrent_coroutines').wait_n
 import time
 import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """
