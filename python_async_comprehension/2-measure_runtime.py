cc55ee402b42807ed09f57aac35621f210898bfc
AdeleM-prog <12043@holbertonstudents.com>
2026-04-10 11:46:59 +0200

    crea 2-measure_runtime + 2-main

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module that provides a coroutine to measure the runtime of
+four parallel async comprehensions.
+"""
+
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime()-> float:
+    """
+    Measures the total runtime of executing async_comprehension
+    four times in parallel.
+    Returns: float: The total runtime in seconds.
+    """
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    return end_time - start_time
