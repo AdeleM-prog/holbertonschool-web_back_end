d16c9a5a51a3c9aa1386e4636801a318d2d9f201
AdeleM-prog <12043@holbertonstudents.com>
2026-04-20 11:36:03 +0200

    add docstrings + pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-""""""
+"""Helper function for pagination."""
 
-def index_range (page, page_size):
+
+def index_range(page, page_size):
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+
+    Args:
+        page (int): the page number (1-indexed)
+        page_size (int): the number of items per page
+
+    Returns:
+        tuple: a tuple (start_index, end_index)
+    """
     offset = (page - 1) * page_size
     end_index = offset + page_size
     return (offset, end_index)
