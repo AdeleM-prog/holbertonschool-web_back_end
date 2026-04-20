9b8cefb53ebaf11e3c410f4e1740df52daed3ff4
AdeleM-prog <12043@holbertonstudents.com>
2026-04-20 11:32:37 +0200

    correction faute de frappe

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 
 """"""
 
-def inex_range (page, page_size):
+def index_range (page, page_size):
     offset = (page - 1) * page_size
     end_index = offset + page_size
     return (offset, end_index)
