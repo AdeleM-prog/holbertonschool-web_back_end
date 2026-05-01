69a30645096e2aa587e11922f25322ec523c5078
AdeleM-prog <12043@holbertonstudents.com>
2026-05-01 17:55:17 +0200

    crea 8-main.js + 8-hbtn_class.js

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,35 @@
+export default class HolbertonClass{
+    constructor(size, location){
+        this._size = size;
+        this._location = location;
+    }
+
+    get size() {
+        return this._size;
+    }
+    set size(value){
+        if (typeof value !== 'number'){
+            throw new TypeError('Size must be a number');
+        }
+        this._size = value;
+    }
+
+    get location() {
+        return this._location;
+    }
+    set location(loc_value){
+        if (typeof loc_value !== 'string'){
+            throw new TypeError('Location must be a string');
+        }
+        this._location = loc_value;
+    }
+
+    [Symbol.toPrimitive](hint) {
+  if (hint === 'number') {
+    return this._size;
+  }
+  if (hint === 'string') {
+    return this._location;
+  }
+}
+}
