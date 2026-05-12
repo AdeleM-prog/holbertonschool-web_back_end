7c4eda72edab557a0e1b9c0d7ce8fea1d830d92d
AdeleM-prog <12043@holbertonstudents.com>
2026-05-12 10:48:49 +0200

    crea 2-read_files.js + 2files main.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,14 +1,31 @@
 const fs = require('fs');
-module.exports = function countStudents(path) {
 
+module.exports = function countStudents(path) {
 try {
   const data = fs.readFileSync(path, 'utf8');
   const lines = data.split('\n');
-  const students = lines.slice(1).filter((line) => line.trim() !== '');
 
-  Parser chaque ligne pour extraire firstname et field
-  const result = Compter le total et grouper par champ
-  console.log(`Number of students: ${result}`);
+  const students = lines
+  .slice(1)
+  .filter((line) => line.trim() !== '')
+  .map((line) => {
+    const values = line.split(',');
+    return {firstname: values[0], field: values[3]};
+  });
+
+  const groups = {};
+
+  students.forEach((student) => {
+    if (groups[student.field] === undefined){
+      groups[student.field] = [];
+    }
+    groups[student.field].push(student.firstname);
+  })
+  console.log(`Number of students: ${students.length}`);
+
+  Object.keys(groups).forEach((field) => {
+    console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`)
+  });
 } catch (err) {
   throw new Error('Cannot load the database');
 }
