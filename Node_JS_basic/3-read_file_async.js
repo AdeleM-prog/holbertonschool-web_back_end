dad324c33583e674b875751b2ed5320e2bb573ff
AdeleM-prog <12043@holbertonstudents.com>
2026-05-13 15:28:43 +0200

    correction eslint

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,37 @@
 const fs = require('fs');
 
-function countStudents(path){
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-                reject(new Error('Cannot load the databse'));
-                return;
-            } else {
-                const lines = data.split('\n');
-                
-                const students = lines
-                .slice(1)
-                .filter((line) => line.trim() !== '')
-                .map((line) => {
-                    const values = line.split(',');
-                    return {firstname: values[0], field: values[3]};
-                });
-                
-                const groups = {};
-                
-                students.forEach((student) => {
-                    if (groups[student.field] === undefined){
-                    groups[student.field] = [];
-                    }
-                    groups[student.field].push(student.firstname);
-                })
-                console.log(`Number of students: ${students.length}`);
-                
-                Object.keys(groups).forEach((field) => {
-                    console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`)
-                });
-            resolve();
-            };
-        })
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the databse'));
+        return;
+      }
+      const lines = data.split('\n');
+
+      const students = lines
+        .slice(1)
+        .filter((line) => line.trim() !== '')
+        .map((line) => {
+          const values = line.split(',');
+          return { firstname: values[0], field: values[3] };
+        });
+
+      const groups = {};
+
+      students.forEach((student) => {
+        if (groups[student.field] === undefined) {
+          groups[student.field] = [];
+        }
+        groups[student.field].push(student.firstname);
+      });
+      console.log(`Number of students: ${students.length}`);
+
+      Object.keys(groups).forEach((field) => {
+        console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
+      });
+      resolve();
     });
+  });
 }
 module.exports = countStudents;
