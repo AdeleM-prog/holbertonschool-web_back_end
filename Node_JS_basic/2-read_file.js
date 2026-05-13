77e5dd63f20129c051acdd89ee6ae4614070a67f
AdeleM-prog <12043@holbertonstudents.com>
2026-05-13 15:16:51 +0200

    correction requirements 2-read_files

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,32 +1,32 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-try {
-  const data = fs.readFileSync(path, 'utf8');
-  const lines = data.split('\n');
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n');
 
-  const students = lines
-  .slice(1)
-  .filter((line) => line.trim() !== '')
-  .map((line) => {
-    const values = line.split(',');
-    return {firstname: values[0], field: values[3]};
-  });
+    const students = lines
+      .slice(1)
+      .filter((line) => line.trim() !== '')
+      .map((line) => {
+        const values = line.split(',');
+        return { firstname: values[0], field: values[3] };
+      });
 
-  const groups = {};
+    const groups = {};
 
-  students.forEach((student) => {
-    if (groups[student.field] === undefined){
-      groups[student.field] = [];
-    }
-    groups[student.field].push(student.firstname);
-  })
-  console.log(`Number of students: ${students.length}`);
+    students.forEach((student) => {
+      if (groups[student.field] === undefined) {
+        groups[student.field] = [];
+      }
+      groups[student.field].push(student.firstname);
+    });
+    console.log(`Number of students: ${students.length}`);
 
-  Object.keys(groups).forEach((field) => {
-    console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`)
-  });
-} catch (err) {
-  throw new Error('Cannot load the database');
-}
-}
+    Object.keys(groups).forEach((field) => {
+      console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
