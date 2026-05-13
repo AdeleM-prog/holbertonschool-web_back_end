dc1649368bcea9f1cbbdc87140e5415206ab9ec0
AdeleM-prog <12043@holbertonstudents.com>
2026-05-13 15:43:04 +0200

    correction output

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,36 +2,32 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the databse'));
-        return;
-      }
-      const lines = data.split('\n');
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const splitlines = data.split('\n');
+        const slicedata = splitlines.slice(1).filter((splitline) => splitline !== '');
+        console.log(`Number of students: ${slicedata.length}`);
 
-      const students = lines
-        .slice(1)
-        .filter((line) => line.trim() !== '')
-        .map((line) => {
-          const values = line.split(',');
-          return { firstname: values[0], field: values[3] };
+        const students = {};
+        slicedata.forEach((splitline) => {
+          const columns = splitline.split(',');
+          const firstname = columns[0];
+          const field = columns[3];
+          if (!students[field]) {
+            students[field] = [];
+          }
+          students[field].push(firstname);
         });
 
-      const groups = {};
-
-      students.forEach((student) => {
-        if (groups[student.field] === undefined) {
-          groups[student.field] = [];
-        }
-        groups[student.field].push(student.firstname);
-      });
-      console.log(`Number of students: ${students.length}`);
-
-      Object.keys(groups).forEach((field) => {
-        console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
-      });
-      resolve();
+        Object.keys(students).forEach((field) => {
+          console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+        });
+        resolve();
+      }
     });
   });
 }
+
 module.exports = countStudents;
